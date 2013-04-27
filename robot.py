7a759e4a0d1330b94f9967e97ca24e9e17f9ddea
guill <guill@guill-PC.home>
2013-04-27 17:31:45 +0200

    ajout coeur

--- robot.py
+++ robot.py
@@ -1,6 +1,6 @@
 import ConfigParser, pyfirmata
 from vision import Vision
-import propulsion, landmap
+import propulsion, landmap, collections, time, thread
 class Robot():
     '''
         classe qui rassemble l'ensemble des modules du robot
@@ -14,3 +14,29 @@ class Robot():
         self.vue = Vision( self.config, self.board )
         self.land = landmap.LandMap()
         self.accel = propulsion.Accel()
+        self.pile = collections.deque()
+        self.pilevar = collections.deque()
+        self.running = False
+
+
+    def start( self ):
+        self.running = True
+        thread.start_new_thread( self.coeur, () )
+
+    def stop( self ):
+        self.running = False
+
+    def add_to_pile( self, exe, var=() ):
+        self.pile.append( exe )
+        self.pilevar( var )
+
+    def __exec_next__( self ):
+        self.pile.popleft()( self.pilevar.popleft() )
+
+    def coeur( self ):
+        while self.running:
+            if self.pile > 0:
+                self.__exec_next__()
+            else:
+                time.sleep( 0.1 )
+            #thread.start_new_thread(gcode.get(ref[0],nullcomm),(ref[1:],))
