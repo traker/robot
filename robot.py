5dd827b2fe36b72536764638760d9c7ff74e4bfc
guill <guill@guill-PC.home>
2013-04-27 13:42:43 +0200

    class robot

---
+++ robot.py
@@ -0,0 +1,15 @@
+import ConfigParser, pyfirmata
+from vision import Vision
+import propulsion, landmap
+class Robot():
+    '''
+        class qui rassemble l'ensemble des modules du robot
+    '''
+    def __init__( self ):
+        self.config = ConfigParser.RawConfigParser()
+        self.config.read( 'RobotConf.cfg' )
+        self.board = pyfirmata.ArduinoMega( config.getint( 'Arduino', 'device' ) )
+        self.prop = propulsion.Propulsion( self.config, self.board )
+        self.tourel = propulsion.Propulsion( self.config, self.board )
+        self.vue = Vision( self.config, self.board )
+        self.land = landmap.LandMap()
