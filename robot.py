06f958c06ca01298e3c1acf14589158b982d9fe1
guill <guill@guill-PC.home>
2013-05-03 17:13:15 +0200

    fix

--- robot.py
+++ robot.py
@@ -27,7 +27,7 @@ class Robot():
         self.orientation = 0.0
         self.x = 0.0
         self.y = 0.0
-        self.stream = stream.HTTPServer( ( "", 8080 ), self.vue )
+        self.stream = stream.HTTPServer( ( "", 8080 ), self.vue, 3 )
 
 
     def start( self ):
