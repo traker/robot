81b1484bdab81c6cdb8dc1db9c7c96860f4a68d1
guill <guill@guill-PC.home>
2013-05-08 15:11:32 +0200

    fix

--- vision.py
+++ vision.py
@@ -92,15 +92,19 @@ class Vision():
 				self.snapshot = self.cam.get_image( self.snapshot )
 				self.snapshot = self.cam.get_image( self.snapshot )
 				self.image_brut = self.__pygame_to_cvimage__( self.snapshot )
+				self.timexe = time.time()
 				print "flush"
 			else:
 				self.image_brut = self.snapshot
+				self.timexe = time.time()
 		else:
 			if self.cam.query_image():
 				self.snapshot = self.cam.get_image( self.snapshot )
 				self.image_brut = self.__pygame_to_cvimage__( self.snapshot )
+				self.timexe = time.time()
 			else:
 				self.image_brut = self.snapshot
+				self.timexe = time.time()
 
 
 	def __img_filter__( self ):
