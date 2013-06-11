f0db97c5615b2b72e353000bb555ae717e32be11
traker <guihej@gmail.com>
2013-06-11 19:26:58 +0200

    ajout fonction get_batt

--- raspberry/i2c_protocole.py
+++ raspberry/i2c_protocole.py
@@ -15,7 +15,7 @@ class I2c_arduino():
 
     CMDX = 0x01
     CMDY = 0x02
-    CMD = 0x00
+
     def __init__( self ):
         self.i2c = Adafruit_I2C( 0x20 )
 
@@ -27,31 +27,29 @@ class I2c_arduino():
         time.sleep( 0.020 )
         self.i2c.write8( self.CMDY, y )
         time.sleep( 0.020 )
-        #self.i2c.write8( self.CMDG, self.TOUREXE )
-        self.CMD += self.TOUREXE
+        self.i2c.write8( self.CMDG, self.TOUREXE )
+        time.sleep( 0.020 )
 
     def avance( self, avar ):
         if( avar ):
             self.i2c.write8( self.CMDG, self.PROPAV )
-            #self.CMD += self.PROPAV
         else:
             self.i2c.write8( self.CMDG, self.PROPAR )
-            #self.CMD += self.PROPAR
+        time.sleep( 0.020 )
 
     def tourne( self, dg ):
         if( dg ):
             self.i2c.write8( self.CMDG, self.TOURND )
-            #self.CMD += self.TOURND
         else:
             self.i2c.write8( self.CMDG, self.TOURNG )
-            #self.CMD += self.TOURNG
+        time.sleep( 0.020 )
 
     def laser( self ):
         self.i2c.write8( self.CMDG, self.LASERON )
-        #self.CMD += self.LASERON
+        time.sleep( 0.020 )
     def raz( self ):
         self.i2c.write8( CMDG, self.RAZ )
-        self.CMD += self.RAZ
+        time.sleep( 0.020 )
 
     def get_Compteur( self ):
         #compteur droite
@@ -72,6 +70,7 @@ class I2c_arduino():
         self.i2c.write8( self.CMDG, self.NIVBATT )
         time.sleep( 0.020 )
         tmp = self.i2c.readList( self.CMDG, 4 )
+        time.sleep( 0.020 )
         cha = chr( tmp[0] ) + chr( tmp[1] ) + chr( tmp[2] ) + chr( tmp[3] )
         struct.unpack( 'f', cha )
 
